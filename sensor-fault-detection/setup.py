c845ce3721f2bd67610e148fda30cf1dc9dc9d62
Vineet <vineet.bhardwaj100@gmail.com>
2022-10-30 09:42:21 +0530

    UPdated Setup.py File

--- sensor-fault-detection/setup.py
+++ sensor-fault-detection/setup.py
@@ -7,6 +7,12 @@ def get_requirements()->List[str]:
     """
     requirement_list:List[str] = []
 
+    requirement_items = open('C:/Users/vinee/PGP-Data Science/Industry-Ready-Project/Sensor-Fault-Detection-System/sensor-fault-detection/requirements.txt', 'r')
+    for items in requirement_items:
+        requirement_list.append(items)
+    
+    return requirement_list
+
     """
     Write a code to read requirements.txt file and append each requirements in requirement_list variable.
     """
@@ -15,8 +21,8 @@ def get_requirements()->List[str]:
 setup(
     name="sensor",
     version="0.0.1",
-    author="ineuron",
-    author_email="avnish@ineuron.ai",
+    author="Vineet Bhardwaj",
+    author_email="vineet.100@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements(),#["pymongo==4.2.0"],
 )
